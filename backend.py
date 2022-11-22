08ddad03fc3769bd76ae6c34dfcdd00495507756
FulinQ <118252237+FulinQ@users.noreply.github.com>
2022-11-22 23:50:10 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import os
 class Product:
   def __init__(self, pr_id, pr_name, pr_cost, pr_price, pr_stock):
     self.pr_id = pr_id
@@ -18,7 +19,6 @@ class Product:
       self.pr_stock -= amount
     else:
       return 'the given amount is higher than the available stock'
-    # save the editing history
 
 class pr_Database:
   def __init__(self):
@@ -27,7 +27,7 @@ class pr_Database:
   def add(self,product):
     self.db.append(product)
 
-  def findid_pr(self, pr_name):
+  def findna_pr(self, pr_name):
     for pr in self.db:
       if pr.pr_name==pr_name:
         return pr
@@ -41,4 +41,52 @@ class pr_Database:
     print("Product not found")
     return None
 
+# def total_stock():
+#   if len(pr_database) == 0:
+#     return 0
+#   elif len(pr_database) < 0:
+#     return 'Error: Negative amount in stock'
+#   elif len(pr_database) > 0:
+#     for 
+
 pr_database = pr_Database()
+os.chdir('/Users/fulinq/Documents/KMITL/Y1/T1/IntroToProgramming/PyProject')
+fi = open('product.txt', 'r')
+pr_count = 1
+while True:
+    lines = fi.readline()
+    if lines == '':
+        break
+    else:
+        pr_data = lines.split()
+        id = int(pr_data[0])
+        name = pr_data[1]
+        cost = int(pr_data[2])
+        price = int(pr_data[3])
+        stock = int(pr_data[4])
+        product = Product(id, name, cost, price, stock)
+        pr_database.add(product)
+        pr_count += 1
+
+print(pr_database)
+
+while True:
+    command = input()
+    #Quit command
+    if command == 'q':
+        break
+    #Find the available stocks with product's id
+    elif command == 'sid':
+        product = pr_database.findid_pr(int(input()))
+        if product is not None:
+            print(product.get_available(), 'in stock')
+        else:
+            print('Invalid product ID')
+    #Find the available stocks with product's id
+    elif command == 'sna':
+        product = pr_database.findna_pr(input())
+        if product is not None:
+            print(product.get_available(), 'in stock')
+        else:
+            print('Invalid product ID')
+      
